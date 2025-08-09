bda582ae771b95d5faf8fd1b0564144d9dc0b9ff
Louise <louisecarigma759@gmail.com>
2025-08-09 17:20:38 +0800

    Just a calc

---
+++ Basic Calculatoohh/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------Basic Calculator------------");
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator (+, -, *, /): ");
+            string opera = Console.ReadLine();
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            bool valid = true;
+
+            switch (opera)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero.");
+                        valid = false;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    valid = false;
+                    break;
+            }
+
+            if (valid)
+                Console.WriteLine($"Result: {result}");
+        }
+    }
+}
